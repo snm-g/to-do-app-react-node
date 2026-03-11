fbc1a07dcfa4555f5872270659dc9d587a653329
Sergio_NM <180465994+snm-g@users.noreply.github.com>
2026-03-11 17:14:03 -0400

    TDLRN-3: Corrections in Migrate file

--- src/db/migrate.js
+++ src/db/migrate.js
@@ -8,18 +8,26 @@ const __dirname = path.dirname(__filename);
 
 const correrMigraciones = async () => {
   try {
-    console.log("Iniciando migración...");
+    console.log("Iniciando sistema de migraciones...");
+    const archivosSql = ["users.sql", "categories.sql", "tags.sql", "tasks.sql", "tags_tasks.sql"];
 
-    const rutaSql = path.join(__dirname, "migrations", "user.sql");
-    const sqlNativo = fs.readFileSync(rutaSql, "utf8");
+    for (const archivo of archivosSql) {
+      const rutaSql = path.join(__dirname, "migrations", archivo);
+      if (fs.existsSync(rutaSql)) {
+        console.log(`Ejecutando ${archivo}...`);
+        const sqlNativo = fs.readFileSync(rutaSql, "utf8");
 
-    await pool.query(sqlNativo);
+        await pool.query(sqlNativo);
+        console.log(`Tabla de ${archivo} lista.`);
+      } else {
+        console.log(`Archivo ${archivo} no encontrado. Saltando...`);
+      }
+    }
 
-    console.log('Migración exitosa: Tabla "users" creada o ya existente.');
-
+    console.log("¡Todas las migraciones terminaron con éxito!");
     process.exit(0);
   } catch (error) {
-    console.error("Error ejecutando la migración:", error.message);
+    console.error("Error fatal ejecutando las migraciones:", error.message);
     process.exit(1);
   }
 };
